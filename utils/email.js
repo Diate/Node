b00cb39e5f876d7e9c44a37a9430b52a282c241a
Diate <radiobuon9@gmail.com>
2024-06-27 23:20:11 +0700

    fix can't send email

--- utils/email.js
+++ utils/email.js
@@ -1,23 +1,21 @@
 const nodemailer = require('nodemailer');
-
 const sendEmail = async (option) => {
-  const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
+  var transport = nodemailer.createTransport({
+    host: 'sandbox.smtp.mailtrap.io',
+    port: 2525,
     auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
+      user: '7a6ea375a0d4fe',
+      pass: '44cf8225a7bec3',
     },
   });
+  console.log(transport);
   const mailOptions = {
-    from: 'TU BAO TRAN <baotutran@email.io>',
+    from: 'TU BAO TRAN <baotutran12@email.io>',
     to: option.email,
     subject: option.subject,
     text: option.message,
   };
   console.log(mailOptions);
-  await transporter.sendMail(mailOptions);
+  transport.sendMail(mailOptions);
 };
-
 module.exports = sendEmail;
